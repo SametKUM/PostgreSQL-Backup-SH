19bfdd062a0305df122ab4e7fdb5b7ca94f1500e
SametKUM <kumsamet@gmail.com>
2020-12-11 21:53:52 +0300

    Comment Fix

--- postgresql_backup.sh
+++ postgresql_backup.sh
@@ -4,7 +4,7 @@ TODAY=`date +%d-%m-%Y_%H-%M.gz`
 # Backup Filename
 ARCHIVE=DB_NAME-$TODAY
 # Backup create
-# PostgreSQL can be installed on the server via the package manager, you can use the pg_dump command. 'su - postgres -c "pg_dump -U postgres AMZCAD | gzip > /mount/dbfiles/$ARCHIVE'
+# PostgreSQL can be installed on the server via the package manager, you can use the pg_dump command. 'su - postgres -c "pg_dump -U postgres DB_NAME | gzip > /mount/dbfiles/$ARCHIVE'
 su - postgres -c "/usr/local/pgsql/bin/pg_dump -U postgres DB_NAME | gzip > /mount/dbfiles/$ARCHIVE"
 #Delete files older than 20 days
 find /mount/dbfiles/ -mtime +20 -delete
